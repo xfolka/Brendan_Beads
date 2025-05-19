6209b2e2428a9e5adc4b4211d4aee0b1d4bc428b
Anders Folkesson <anders.folkesson@gu.se>
2025-05-19 15:42:02 +0200

    removed unused stuff

--- src/brendan_beads/__init__.py
+++ src/brendan_beads/__init__.py
@@ -4,18 +4,15 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
-from ._writer import write_multiple, write_single_image
+# from ._reader import napari_get_reader
+# from ._sample_data import make_sample_data
+from ._widget import MainWidget
+# from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+#    "napari_get_reader",
+    # "write_single_image",
+    # "write_multiple",
+    # "make_sample_data",
+    "MainWidget",
 )
