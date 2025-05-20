d63b44b1720538e3aab532bb048448d5c5f0b5d1
Anders Folkesson <anders.folkesson@gu.se>
2025-05-20 10:50:35 +0200

    removed tests

--- src/brendan_beads/__init__.py
+++ src/brendan_beads/__init__.py
@@ -2,7 +2,7 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "0.0.1"
 
 # from ._reader import napari_get_reader
 # from ._sample_data import make_sample_data
--- src/brendan_beads/_tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from brendan_beads import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
