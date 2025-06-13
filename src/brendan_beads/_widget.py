fixed channels, and scaling

src/brendan_beads/_widget.py
@@ -1,10 +1,25 @@
-from typing import TYPE_CHECKING, Optional
-
-from qtpy.QtCore import Signal, Qt
-from qtpy.QtWidgets import QVBoxLayout, QPushButton, QWidget, QFileDialog, QProgressDialog, QLabel, QSpinBox, QMessageBox
-from .generate_data import load_and_generate_data, generate_and_visualize_future, read_ome_tiff
-import numpy as np
+import itertools
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+import numpy as np
+from qtpy.QtCore import Qt, Signal
+from qtpy.QtWidgets import (
+    QComboBox,
+    QFileDialog,
+    QLabel,
+    QMessageBox,
+    QProgressDialog,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+from .generate_data import (
+    generate_and_visualize_future,
+    get_metada_pixelsizes,
+    read_ome_tiff,
+)
 
 if TYPE_CHECKING:
     import napari
@@ -12,11 +27,7 @@ if TYPE_CHECKING:
 
 class MainWidget(QWidget):
 
-
-    Z_AXIS = 1
-    C_AXIS = 0
-
-    show_all_layers_signal = Signal(np.ndarray, str, np.ndarray, str, np.ndarray, str, np.ndarray, str, int)
+    show_all_layers_signal = Signal(np.ndarray, str, np.ndarray, str, np.ndarray, str, np.ndarray, str, np.ndarray)
     close_progress_signal = Signal()
     
     # use a type annotation of 'napari.viewer.Viewer' for any parameter
@@ -27,78 +38,57 @@ class MainWidget(QWidget):
         self.loadImageBtn = QPushButton("Load image")
         self.loadImageBtn.clicked.connect(self._on_load_image)
 
-        self.z_spin = QSpinBox()
-        self.z_spin.setMinimum(0)
-        self.z_spin.setReadOnly(True)
+        self.surface_combo = QComboBox()
 
-        self.c_spin = QSpinBox()
-        self.c_spin.setMinimum(0)
-        self.c_spin.setReadOnly(True)
+        self.beads_combo = QComboBox()
 
         self.readDataBtn = QPushButton("Start visualization")
         self.readDataBtn.clicked.connect(self._on_start_computing)
         self.readDataBtn.setEnabled(False)
 
-        self.progress_dialog = QProgressDialog("","",0,0)
+        self.progress_dialog = QProgressDialog("", "", 0, 0)
         self.progress_dialog.setAutoClose(True)
         self.progress_dialog.reset()
 
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.loadImageBtn)
-        self.layout().addWidget(QLabel("Selected Z"))
-        self.layout().addWidget(self.z_spin)
-        # self.layout().addWidget(QLabel("Selected C"))
-        # self.layout().addWidget(self.c_spin)
-
+        self.layout().addWidget(QLabel("Choose surface layer"))
+        self.layout().addWidget(self.surface_combo)
+        self.layout().addWidget(QLabel("Choose beads layer"))
+        self.layout().addWidget(self.beads_combo)
         self.layout().addWidget(self.readDataBtn)
+
         self.dataFile = None
+        self.image_path: Path | None = None
 
         self.show_all_layers_signal.connect(self._show_all_layers,Qt.QueuedConnection)
         self.close_progress_signal.connect(self._close_progress_dialog, Qt.QueuedConnection)
-        #viewer.layers.events.inserted.connect(self._image_loaded)
-        viewer.dims.events.current_step.connect(self._current_z_or_c_changed)
-        
-        self.image_path: Path | None = None
-        
-#        viewer.layers.selection.events.active.connect(self._on_active_layer_changed)
-
-    # def _on_active_layer_changed(self, event):
-    #     active_layer = event.value  # The newly selected layer (or None)
-    #     print(f"Selected layer changed to: {active_layer}")
-
-    def _current_z_or_c_changed(self, event):
-
-        self.z_spin.setValue(event.value[MainWidget.Z_AXIS])
-        self.c_spin.setValue(event.value[MainWidget.C_AXIS])
-        #print("jek")
 
     def _on_load_image(self):
         data = QFileDialog.getOpenFileName()
         if not data[0]:
             return
+        
+        if len(self.viewer.layers) > 0 and QMessageBox.question(None, "Clear data?", "All layers will be cleared before loading image. OK?") != QMessageBox.Yes:
+            return
+        
+        self.viewer.layers.clear()
+        self.beads_combo.clear()
+        self.surface_combo.clear()
         self.image_path = Path(data[0])
         img, mdata = read_ome_tiff(self.image_path)
+        psizes = get_metada_pixelsizes(mdata)
+        xs = psizes['PhysicalSizeX']
+        ys = psizes['PhysicalSizeY']
+        zs = psizes['PhysicalSizeZ']
+        self.metadata = mdata
+        name_generator = (f"channel {x}" for x in itertools.count())
         
-        self.viewer.add_image(img, metadata=mdata)
+        self.viewer.add_image(img, metadata=mdata, channel_axis=0, name=name_generator, scale=(zs, ys, xs))
+        for ll in self.viewer.layers:
+            self.beads_combo.addItem(ll.name)
+            self.surface_combo.addItem(ll.name)
         
-        self.readDataBtn.setEnabled(True)
-        z = img.shape[MainWidget.Z_AXIS] if img.ndim == 4 else 0
-        self.z_spin.setMaximum(z)
-
-    def _image_loaded(self, event):
-        #print(event)
-        if self.image_path is not None:
-            return QMessageBox.warning(None, "Use only one file", "This plugin will only use the first file loaded")
-            
-        self.image_path = Path(event.value.source.path)
-        if event.source.ndim == 4: #change to event.source.ndim
-            z = event.value.data.shape[MainWidget.Z_AXIS]
-        else:
-            z = 0
-        c = event.value.data.shape[MainWidget.C_AXIS]
-
-        self.z_spin.setMaximum(z)
-        self.c_spin.setMaximum(c)
         self.readDataBtn.setEnabled(True)
 
     def _on_start_computing(self):
@@ -107,21 +97,29 @@ class MainWidget(QWidget):
 
     def _show_all_layers(self, i, ii, s, si, b, bi, v, vi, scale):
 
-        self.viewer.add_image(i, name=ii)
-        self.viewer.add_labels(s, name=si)
-        self.viewer.add_labels(b, name=bi)
-        self.viewer.add_vectors(v, name=vi, edge_color='lime')
+        self.viewer.add_image(i, name=ii, scale=scale)
+        self.viewer.add_labels(s, name=si, scale=scale)
+        self.viewer.add_labels(b, name=bi, scale=scale)
+        self.viewer.add_vectors(v, name=vi, edge_color='lime', scale=scale)
 
         #set 3d view mode
         self.viewer.dims.ndisplay = 3
 
-    def run_all(self, file: Path):
-        img_data = self.viewer.layers[0].data
-        metadata = self.viewer.layers[0].metadata
-        z = self.z_spin.value()
-        c = self.c_spin.value()
+    def _get_layer_by_name(self, name: str):
+        for ll in self.viewer.layers:
+            if ll.name == name:
+                return ll
+            
+        return None
 
-        future = generate_and_visualize_future(img_data[:, z, :, :], metadata, file)
+    def run_all(self, file: Path):
+        surface = self._get_layer_by_name(self.surface_combo.currentText())
+        beads = self._get_layer_by_name(self.beads_combo.currentText())
+
+        if surface is None or beads is None:
+            return QMessageBox.warning(None, "No data", "No layers to generate data from!")
+        
+        future = generate_and_visualize_future(beads.data, surface.data, self.metadata, file)
         
         f_cb = self._future_done_callback
         future.add_done_callback(f_cb)

src/brendan_beads/generate_data.py
@@ -5,22 +5,22 @@ import numpy as np
 import pandas as pd
 from pyometiff import OMETIFFReader, OMETIFFWriter
 from scipy.ndimage import gaussian_filter
-from scipy.spatial import cKDTree
+from scipy.spatial import KDTree
 from skimage.filters import threshold_otsu
 from skimage.measure import label, regionprops
-from skimage.morphology import ball, disk, ellipse, opening
+from skimage.morphology import ellipse, opening
 from skimage.segmentation import find_boundaries
 
 DATA_GEN_DIR = "_generated"
 
 
-def generate_and_visualize_future(img_data, meta_data, filePath: pathlib.Path):
-    future = ThreadPoolExecutor().submit(generate_and_visualize, img_data, meta_data, filePath)
+def generate_and_visualize_future(blobs_data_layer, surface_data_layer, meta_data, filePath: pathlib.Path):
+    future = ThreadPoolExecutor().submit(generate_and_visualize, blobs_data_layer, surface_data_layer, meta_data, filePath)
     return future
 
 
-def generate_and_visualize(img_data, metadata, filePath: pathlib.Path):
-    generate_data(img_data, metadata, filePath)
+def generate_and_visualize(blobs_data_layer, surface_data_layer, metadata, filePath: pathlib.Path):
+    generate_data(blobs_data_layer, surface_data_layer, metadata, filePath)
     
     #load_and_generate_data(filePath)
     return get_layers_to_visualize(filePath)
@@ -40,33 +40,30 @@ def get_and_create_data_dir(filePath: pathlib.Path):
     return g_dir_path
 
 
-def generate_data(image_data, metadata, filePath: pathlib.Path):
+def get_metada_pixelsizes(metadata: dict) -> dict:
+    metadata_dict = {
+        "PhysicalSizeX": metadata['PhysicalSizeX'],
+        "PhysicalSizeXUnit": "µm",
+        "PhysicalSizeY": metadata['PhysicalSizeY'],
+        "PhysicalSizeYUnit": "µm",
+        "PhysicalSizeZ": metadata['PhysicalSizeZ'],
+        "PhysicalSizeZUnit": "µm"
+    }
+    return metadata_dict
+
+
+def generate_data(blobs_data_layer, surface_data_layer, metadata, filePath: pathlib.Path):
     
     g_dir_path = get_and_create_data_dir(filePath)
     fname = filePath.name
     
-    blobs = generate_blobs_data_for(image_data[0], metadata, fname, g_dir_path)
-    surfaces = generate_surfaces_data_for(image_data[1], metadata, fname, g_dir_path)
+    blobs = generate_blobs_data_for(blobs_data_layer, metadata, fname, g_dir_path)
+    surfaces = generate_surfaces_data_for(surface_data_layer, metadata, fname, g_dir_path)
     generate_vectors_data_for(blobs, surfaces, metadata, fname, g_dir_path)
 
 
-def load_and_generate_data(filePath: pathlib.Path):
-
-    #create data generation directory
-    g_dir = get_generated_data_dir(filePath)
-    g_dir_path = filePath.parent.joinpath(g_dir)
-    g_dir_path.mkdir(exist_ok=True)
-    # Load the full z stack, this will be handled later by napari
-    reader = OMETIFFReader(fpath=filePath)
-    img_array, metadata, xml_metadata = reader.read()
-
-    channel_0 = img_array[0]  # shape: (140, 512, 512)
-    channel_1 = img_array[1]  # shape: (140, 512, 512)
-
-
-#MAKE SURE YOU SEND CHANNEL 0 HERE
 def generate_blobs_data_for(img_data, metadata, file_name, g_dir_path):
-    ch1_filtered = gaussian_filter(img_data, sigma=[2.,1.,1.])
+    ch1_filtered = gaussian_filter(img_data, sigma=[2., 1., 1.])
     filt_op = np.max(ch1_filtered, axis=2)
     thresh = threshold_otsu(filt_op)
 
@@ -76,35 +73,18 @@ def generate_blobs_data_for(img_data, metadata, file_name, g_dir_path):
     assert blobs.max()<65000, "issues here"
     blobs = blobs.astype(np.uint16)
 
-    # # `blobs` is a labeled 3D array
-    # props = regionprops(blobs)
-
-    # # Extract centroids
-    # center_coords = np.array([p.centroid for p in props])  # shape: (N, 3), in (z, y, x)
-
-    #fname = filePath.name
-    #fdir = filePath.absolute()
     fname = file_name.split(".ome.tiff")[0]
     fname = fname+"_blobs.ome.tiff"
     omePath = g_dir_path.joinpath(fname)
-    #print(omePath)
 
     dimension_order = "ZYX"
-    metadata_dict = {
-        "PhysicalSizeX" : metadata['PhysicalSizeX'],
-        "PhysicalSizeXUnit" : "µm",
-        "PhysicalSizeY" : metadata['PhysicalSizeY'],
-        "PhysicalSizeYUnit" : "µm",
-        "PhysicalSizeZ" : metadata['PhysicalSizeZ'],
-        "PhysicalSizeZUnit" : "µm",
-
-        "Channels" : {
-            "filtered" : {
-                "Name" : "blobs",
-                "SamplesPerPixel": 1,
-            },
-        }
-    }
+    metadata_dict = get_metada_pixelsizes(metadata)
+    metadata_dict["Channels"] = {
+                "filtered": {
+                    "Name": "blobs",
+                    "SamplesPerPixel": 1
+                }
+            }
 
     writer = OMETIFFWriter(
         fpath=omePath,
@@ -118,7 +98,6 @@ def generate_blobs_data_for(img_data, metadata, file_name, g_dir_path):
     return blobs
 
 
-#MAKE SURE YOU SEND CHANNEL 1 HERE
 def generate_surfaces_data_for(img_data, metadata, file_name, g_dir_path):
 
     def keep_largest_label(label_img):
@@ -161,28 +140,18 @@ def generate_surfaces_data_for(img_data, metadata, file_name, g_dir_path):
     surface = surface.astype(np.uint16)
 
     surface = keep_largest_label(surface)
-    #fname = filePath.name
     fname = file_name.split(".ome.tiff")[0]
     fname = fname+"_surface.ome.tiff"
     omePath = g_dir_path.joinpath(fname)
-    #print(omePath)
 
     dimension_order = "ZYX"
-    metadata_dict = {
-        "PhysicalSizeX" : metadata['PhysicalSizeX'],
-        "PhysicalSizeXUnit" : "µm",
-        "PhysicalSizeY" : metadata['PhysicalSizeY'],
-        "PhysicalSizeYUnit" : "µm",
-        "PhysicalSizeZ" : metadata['PhysicalSizeZ'],
-        "PhysicalSizeZUnit" : "µm",
-
-        "Channels" : {
-            "filtered" : {
-                "Name" : "surface",
-                "SamplesPerPixel": 1,
-            },
-        }
-    }
+    metadata_dict = get_metada_pixelsizes(metadata)
+    metadata_dict["Channels"] = {
+                "filtered": {
+                    "Name": "surface",
+                    "SamplesPerPixel": 1
+                }
+            }
 
     writer = OMETIFFWriter(
         fpath=omePath,
@@ -215,7 +184,7 @@ def generate_vectors_data_for(blobs_img_data, surface_img_data, metadata, file_n
     center_coords_phys = center_coords * voxel_size
 
     # Build KD-tree in physical space
-    tree = cKDTree(edge_coords_phys)
+    tree = KDTree(edge_coords_phys)
     dists, indices = tree.query(center_coords_phys, k=1)
 
     # Find closest edge point for each center
@@ -259,21 +228,15 @@ def get_layers_to_visualize(imgPath : pathlib.Path):
 
     reader = OMETIFFReader(fpath=imgPath)
     img_array, metadata, xml_metadata = reader.read()
-    #nframes = img_array.shape[0]
 
     channel_0 = img_array[0]  # shape: (140, 512, 512)
-    #channel_1 = img_array[1]  # shape: (140, 512, 512)
-
-    #channel_0.shape
 
     #get data generate dir
     dataGenDir = get_generated_data_dir(imgPath)
     surface_path = dataGenDir.joinpath(f"{img_name}_surface.ome.tiff")
 
-    #surface_path = pathlib.Path(f"./data/{img_name}_surface.ome.tiff")
     reader = OMETIFFReader(fpath=surface_path)
     surface, metadata, xml_metadata = reader.read()
-    #surface.shape
 
     blobs_path = dataGenDir.joinpath(f"{img_name}_blobs.ome.tiff")
     reader = OMETIFFReader(fpath=blobs_path)